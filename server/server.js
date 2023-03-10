95dbdcbafc94f1578173e3c3c04605b319d3c194
James Jacobsen <edjcjacobsen@gmail.com>
2023-03-10 14:59:50 -0600

    setup server to run with nodemon, installed dependencies(express, body-parser, pg)

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 5000;
+//const koalaRouter = require('./routes/koala.router');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('server/public'));
+app.use(bodyParser.json());
+
+// ROUTES
+//app.use('/Koala', koalaRouter)
+
+// Start listening for requests on a specific port
+app.listen(PORT, () => {
+  console.log('listening on port', PORT);
+});
