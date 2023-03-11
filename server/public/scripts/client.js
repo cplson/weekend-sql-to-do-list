a01ae13cf3f9c111d98a2f88820c18c949823523
James Jacobsen <edjcjacobsen@gmail.com>
2023-03-10 19:31:07 -0600

    Added GET request, tested handshake works, and added renderTasks function that runs inside of getTasks function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,35 @@ $(document).ready(onReady);
 
 function onReady(){
     console.log('client and jquery loaded');
+
+    // Display current tasks
+    getTasks();
+}
+
+function getTasks(){
+    // Make ajax request to GET tasks from "to_do" db
+    $.ajax({
+        method: 'GET',
+        url: '/to_do' 
+    }).then(response => {
+        console.log('Got tasks from the server');
+        renderTasks(response);
+    }).catch(err => {
+        alert('there was an issue getting the tasks from the server', err);
+    });
+}
+
+function renderTasks(taskList){
+    // For every task:
+    for(let task of taskList){
+        // Target the thead element by id to render tr element
+        $('#tableBody').append(`
+            <tr>
+                <td id="displayIsCompleted"></td>
+                <td id="displayTask">${task.task}</td>
+                <td><button class="completed">Completed</button></td>
+                <td><button class="delete">Delete</button></td>
+            </tr>
+        `);
+    }
 }
