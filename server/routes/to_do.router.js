2da943ab8309d32a5cdcbc9ec2eb36eae0260880
James Jacobsen <edjcjacobsen@gmail.com>
2023-03-14 12:39:41 -0500

    moved PORT declaration to see if that fixed deployment issue

--- server/routes/to_do.router.js
+++ server/routes/to_do.router.js
@@ -6,12 +6,13 @@ const pool = require('../modules/pool.js');
 toDoRouter.get("/", (req, res) => {
     console.log('inside GET route');
     const queryText = `SELECT * FROM "to_do";`;
+    
     pool.query(queryText)
         .then(response => {
             console.log('got tasks from the db');
             res.send(response.rows);
         }).catch(err => {
-            console.log('there was an error getting data from server');
+            console.log('there was an error getting data from server', err);
             res.sendStatus(500);
         });
 })
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const toDoRouter = require('./routes/to_do.router.js');
-const PORT = process.env.PORT || 5000;
+
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
@@ -12,6 +12,7 @@ app.use(bodyParser.json());
 app.use('/to_do', toDoRouter)
 
 // Start listening for requests on a specific port
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log('listening on port', PORT);
 });
